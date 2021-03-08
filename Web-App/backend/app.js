8cef4d5a72794f93ca86e50e56d842b5859dd60b
Haaris <haarisj22@hotmail.ca>
2021-03-07 21:31:47 -0500

    Add python script to filter ecg data

--- Web-App/backend/app.js
+++ Web-App/backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const db = require("./db");
 
 const app = express();
+const { spawn } = require("child_process");
 
 app.use(cors());
 app.use(express.json());
@@ -58,14 +59,37 @@ app.get("/realset", (req, res) => {
 
 app.get("/realset/:id", (req, res) => {
   const id = req.params.id;
+
   try {
     db.query(
       `SELECT * FROM realset INNER JOIN realset_data ON realset.id = realset_data.id WHERE realset.id=${id}`,
       (err, result) => {
-        res.status(200).json({
-          // numResults: result.length,
-          data: result,
+        let dataGet = "";
+        // spawn new child process to call the python script
+        // const python = spawn("python", ["ecg_filter.py"]);
+        const python = spawn("python", ["ecg_filter.py"]);
+
+        // collect data from script
+        python.stdout.on("data", function (data) {
+          console.log("Pipe data from python script ...");
+          dataGet += data.toString();
         });
+
+        // in close event we are sure that stream from child process is closed
+        python.stdout.on("end", (code) => {
+          console.log(`child process close all stdio with code ${code}`);
+          res.status(200).json({
+            data: JSON.parse(dataGet),
+            // data: dataGet,
+          });
+        });
+        //send unfiltered data from db to python subprocess
+        python.stdin.write(
+          // JSON.stringify(result.map((x) => x.value * (3.3 / 4096)))
+          JSON.stringify(result.map((x) => x.value))
+          // JSON.stringify()
+        );
+        python.stdin.end();
       }
     );
   } catch (err) {
---
+++ Web-App/backend/ecg_filter.py
@@ -0,0 +1,44 @@
+import sys
+import json
+import numpy as np
+from scipy import signal
+import time
+
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
+    filt_data = signal.filtfilt(b, a, data)
+    return filt_data
+
+def notch_filter(data, notch_freq, fs):
+    Fnotch = 60; # Notch Frequency
+    Q = 25; # Q factor
+    [b, a] = signal.iirnotch (notch_freq, Q, fs);
+    filt_data = signal.filtfilt(b, a, data);
+    return filt_data
+
+def filter_ECG(data, Fs):
+    filtered_data = butter_highpass_filter(data, 0.5, Fs, 5)
+    filtered_data = notch_filter(filtered_data, 60, Fs);
+    return filtered_data
+
+Fs = 300
+np.set_printoptions(threshold=sys.maxsize) #prevent np array truncated print
+
+lines = sys.stdin.readlines()
+loaded = json.loads(lines[0])
+filtered_data = filter_ECG(loaded, Fs)
+filtered_data = np.around(filtered_data, decimals=2)
+
+# comma_delimited_data = [str(el) for el in filtered_data]
+# comma_delimited_data = "[" + ",".join(comma_delimited_data) + "]"
+comma_delimited_data = "[" + ",".join(map(str, filtered_data)) + "]"
+
+
+# print(comma_delimited_data[0:8000])
+# print(comma_delimited_data[7985:8020])
+# print(comma_delimited_data[8001:16000])
+print(comma_delimited_data)
+
+# print(json.dumps('data'))
